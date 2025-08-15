0745de16d94ccccad3b4f9e2790e12556909ba3d
Hamza Manzoor <hamza.manzoor@techmatetech.com>
2025-08-15 23:18:58 +0500

    remove liit restrection from preferences

--- src/preferences/entities/update-preferences.dto.ts
+++ src/preferences/entities/update-preferences.dto.ts
@@ -1,15 +1,12 @@
-import { Length, IsString } from 'class-validator';
+import { IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdatePreferencesDto {
   @ApiProperty({
     description: 'this is name for preferences',
     example: 'sample',
-    minLength: 3,
-    maxLength: 50,
     required: true,
   })
   @IsString()
-  @Length(3, 50)
   name: string;
 }
